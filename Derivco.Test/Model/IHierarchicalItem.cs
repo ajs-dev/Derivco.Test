57bd798b571f43528b579621e8ae14afcc86065c
ajs-dev <dev@gnosistem.com>
2018-11-12 19:34:21 +0000

    init commit

---
+++ Derivco.Test/Model/IHierarchicalItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Derivco.Test.Model
+{
+    public interface IHierarchicalItem
+    {
+        int Id { get; set; }
+        int ParentId { get; set; }
+
+        IList<IHierarchicalItem> Children { get; }
+    }
+}
---
+++ Derivco.Test/Model/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Derivco.Test.Model
+{
+    public class MenuItem : IHierarchicalItem
+    {
+        public MenuItem()
+        {
+            Children = new List<IHierarchicalItem>();
+        }
+
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public IList<IHierarchicalItem> Children { get; private set; }
+
+        public string Name { get; set; }
+    }
+}
